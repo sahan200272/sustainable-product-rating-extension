55bf045c6471da48d83a01c779b6c657b888498b
Mona Lisa <edu.sahan@gmail.com>
2026-02-18 15:38:42 +0530

    configure tailwind CSS and create vite project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-4xl font-bold text-green-700 underline">
+        Tailwind is Working ✅
+      </h1>
+    </div>
+  );
+}
+
+export default App;
