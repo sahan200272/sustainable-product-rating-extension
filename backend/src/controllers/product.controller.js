7ac4350dd77b236ebcb87b41d4aabeebae29ebd5
Mona Lisa <edu.sahan@gmail.com>
2026-02-24 10:33:28 +0530

    implement seperate function to calculate sustainability score of product.

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,14 +1,16 @@
 import * as productService from "../services/product.service.js";
+import calculateSustainabilityScore from "../utils/calculateSustainabilityScore.js";
 import cloudinaryUpload from "../utils/cloudinaryUpload.js";
 
 export const createProduct = async (req, res) => {
   try {
-    const { name, brand, category, description, sustainability, sustainabilityScore } = req.body;
+    const { name, brand, category, description, sustainability } = req.body;
     const files = req.files;
 
     let images;
     //console.log(req.body);
     //console.log("files", files);
+    //console.log("sustainability obj ",sustainability);
 
     // Basic validation
     if (!name || !brand || !category || !description || !sustainability) {
@@ -20,23 +22,25 @@ export const createProduct = async (req, res) => {
 
     // photos upload to the cloudinary and take url and public_if from cloudinary
     if (req.files && req.files.length > 0) {
-
       // call the seperate function to handle photo upload logic
       images = await cloudinaryUpload(req.files);
+
     } else {
       console.log("No file uploaded.");
     }
 
+    const score = calculateSustainabilityScore(sustainability);
+
     //console.log("images array ", images);
 
     const product = await productService.createProduct({
-      name,
-      brand,
-      category,
-      description,
-      images,
-      sustainability,
-      sustainabilityScore
+      name: name,
+      brand: brand,
+      category: category,
+      description: description,
+      images: images,
+      sustainability: sustainability,
+      sustainabilityScore : score
     });
 
     return res.status(201).json({
