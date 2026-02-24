2a7295f8e32c905eae4c257b6aeb59ae81f41dc3
Mona Lisa <edu.sahan@gmail.com>
2026-02-24 16:12:30 +0530

    implement product delete method for delete products from mongo DB

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -108,19 +108,54 @@ export const getSingleProduct = async(req, res) => {
 
 export const updateProduct = async(req, res) => {
 
-  const {id} = req.params;
-  const files = req.files;
-  const data = req.body;
+  try {
+    const {id} = req.params;
+    const files = req.files;
+    const data = req.body;
 
-  //console.log("ID", id);
-  //console.log("files", files);
-  //console.log("Data", data);
+    //console.log("ID", id);
+    //console.log("files", files);
+    //console.log("Data", data);
 
-  const updatedProduct = await productService.updateProduct(id, data, files);
+    const updatedProduct = await productService.updateProduct(id, data, files);
 
-  return res.status(201).json({
-    success: true,
-    messgae: "Product updated successfully",
-    updatedData: updatedProduct
-  })
+    return res.status(201).json({
+      success: true,
+      messgae: "Product updated successfully",
+      updatedData: updatedProduct
+    });
+
+  } catch (error) {
+    console.log("[ProductController] Product Update Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server error while updating product"
+    });
+  }
+}
+
+export const deleteProduct = async(req, res) => {
+
+  try {
+
+    const {id} = req.params;
+    console.log(id);
+    
+    const product = await productService.deleteProduct(id);
+
+    return res.status(201).json({
+      success: true,
+      message: "Product deleted successfully",
+      data: product
+    });
+  
+  } catch (error) {
+    console.log("[ProductController] Product delete Error:", error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server error while retriving a product"
+    })
+  }
 }
