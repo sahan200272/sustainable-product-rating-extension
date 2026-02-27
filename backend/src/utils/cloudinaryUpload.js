5fd44fded6a76c8ac565768bbeab6eed102ad40b
Mona Lisa <edu.sahan@gmail.com>
2026-02-27 21:49:45 +0530

    add proper comments

--- backend/src/utils/cloudinaryUpload.js
+++ backend/src/utils/cloudinaryUpload.js
@@ -5,13 +5,17 @@ const cloudinaryUpload = async(files) => {
     const images = [];
 
     for (const file of files) {
+
+        // convert file into base 64 format
         const base64Photo = `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
 
+        // upload file into cloudinary
         const uploadedPhoto = await cloudinary.uploader.upload(base64Photo, {
             resource_type: "auto",
             folder: "products/photos"
         });
 
+        // push cloudinary public_id and url into array
         images.push({
             public_id: uploadedPhoto.public_id,
             url: uploadedPhoto.secure_url
